368979e7fdbb2601b1518f33272731fce32a4cc6
Brice MOREL <brice@alnitaka.com>
2026-02-09 14:22:33 +0100

    mise à jour pour installation rapide

--- src/main.py
+++ src/main.py
@@ -7,12 +7,6 @@ from config import GPIO_CAPTEUR, GPIO_RESET
 
 
 def main():
-#     init_gpio()
-#
-#
-# GPIO.add_event_detect(GPIO_CAPTEUR, GPIO.RISING, callback=on_panier)
-# GPIO.add_event_detect(GPIO_RESET, GPIO.FALLING, callback=on_reset, bouncetime=200)
-
     try:
         app.run(host="0.0.0.0", port=5000)
     finally:
