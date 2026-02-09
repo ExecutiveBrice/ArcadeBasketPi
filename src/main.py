1eb497f01c520bcbe8112672ddb5c00f9cfaa5c8
Brice MOREL <brice@alnitaka.com>
2026-02-09 14:24:55 +0100

    mise à jour pour installation rapide

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
-from hardware.gpio import cleanup
+from hardware.gpio import init_gpio, cleanup
+from hardware.capteur import on_panier
+from hardware.bouton import on_reset
 from app import app
+import RPi.GPIO as GPIO
+from config import GPIO_CAPTEUR, GPIO_RESET
+
 
 def main():
+    print("GPIO désactivé")
+#     init_gpio()
+#
+#
+# GPIO.add_event_detect(GPIO_CAPTEUR, GPIO.RISING, callback=on_panier)
+# GPIO.add_event_detect(GPIO_RESET, GPIO.FALLING, callback=on_reset, bouncetime=200)
+
+
     try:
         app.run(host="0.0.0.0", port=5000)
     finally:
-        cleanup()
+        print("GPIO désactivé")
+        # cleanup()
 
 
 if __name__ == "__main__":
