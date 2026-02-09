5ec0695b78b75c92e260f90d100f381340c57525
Brice MOREL <brice@alnitaka.com>
2026-02-09 14:11:50 +0100

    mise à jour pour installation rapide

--- src/storage/db.py
+++ src/storage/db.py
@@ -1,6 +1,8 @@
 import sqlite3
+import os
 
-DB_PATH = "storage/database.db"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "storage", "database.db")
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
